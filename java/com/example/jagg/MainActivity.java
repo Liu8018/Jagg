af0b8c77a540d3d95dd9f35e868f74018fa1fcf4
liu <liuyiguang8018@gmail.com>
2019-12-08 00:00:15 +0800

    ...

--- java/com/example/jagg/MainActivity.java
+++ java/com/example/jagg/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
@@ -43,44 +44,10 @@ public class MainActivity extends AppCompatActivity {
         //图标和文字放在GridLayout中
         GridLayout grid = (GridLayout) findViewById(R.id.mainPage_layout);
 
-        //尝试动态添加grid的子布局
-        //10dp=1pixels
-        float pixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
-
-        ImageView iv = new ImageView(this);
-        iv.setId(R.id.reservedId);
-        iv.setImageResource(R.drawable.bit);
-        RelativeLayout.LayoutParams imageViewParams = new RelativeLayout.LayoutParams(
-                (int)(5*pixels),(int)(5*pixels));
-        imageViewParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-        imageViewParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        imageViewParams.setMargins((int)(0.8*pixels),(int)(0.8*pixels),(int)(0.8*pixels),(int)(0.8*pixels));
-        iv.setLayoutParams(imageViewParams);
-
-        TextView tv = new TextView(this);
-        tv.setText("TEST");
-        tv.setContentDescription("http://www.bit.edu.cn/");
-        RelativeLayout.LayoutParams textViewParams = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
-        textViewParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        textViewParams.addRule(RelativeLayout.BELOW,R.id.reservedId);
-        tv.setLayoutParams(textViewParams);
-
-        RelativeLayout rv = new RelativeLayout(this);
-        GridLayout.LayoutParams subGridParams = new GridLayout.LayoutParams(
-                GridLayout.spec(GridLayout.UNDEFINED,GridLayout.FILL,1f),
-                GridLayout.spec(GridLayout.UNDEFINED,GridLayout.FILL,1f));
-        subGridParams.setGravity(Gravity.CENTER_HORIZONTAL);
-        rv.addView(iv);
-        rv.addView(tv);
-        rv.setLayoutParams(subGridParams);
-
-        grid.addView(rv);
-
         //GridLayout的子布局数目
-        int childCount = grid.getChildCount();
+        final int childCount = grid.getChildCount();
 
-        for (int i= 0; i < childCount; i++){
+        for (int i= 0; i < childCount-1; i++){
             //找到第i个图标
             final RelativeLayout container = (RelativeLayout) grid.getChildAt(i);
 
@@ -99,6 +66,49 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+
+        //最后一个图标（添加网站）的点击事件
+        final RelativeLayout container = (RelativeLayout) grid.getChildAt(childCount-1);
+        container.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                //10dp=1pixels
+                float pixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
+
+                //ImageView
+                ImageView iv = new ImageView(MainActivity.this);
+                iv.setId(R.id.reservedId);
+                iv.setBackgroundColor(Color.YELLOW);
+                RelativeLayout.LayoutParams imageViewParams = new RelativeLayout.LayoutParams(
+                        (int)(5*pixels),(int)(5*pixels));
+                imageViewParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+                imageViewParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+                imageViewParams.setMargins((int)(0.8*pixels),(int)(0.8*pixels),(int)(0.8*pixels),(int)(0.8*pixels));
+                iv.setLayoutParams(imageViewParams);
+
+                //TextView
+                TextView tv = new TextView(MainActivity.this);
+                tv.setText("TEST");
+                RelativeLayout.LayoutParams textViewParams = new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+                textViewParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+                textViewParams.addRule(RelativeLayout.BELOW,R.id.reservedId);
+                tv.setLayoutParams(textViewParams);
+
+                //组合成RelativeLayout
+                RelativeLayout rv = new RelativeLayout(MainActivity.this);
+                GridLayout.LayoutParams subGridParams = new GridLayout.LayoutParams(
+                        GridLayout.spec(GridLayout.UNDEFINED,GridLayout.FILL,1f),
+                        GridLayout.spec(GridLayout.UNDEFINED,GridLayout.FILL,1f));
+                subGridParams.setGravity(Gravity.CENTER_HORIZONTAL);
+                rv.addView(iv);
+                rv.addView(tv);
+                rv.setLayoutParams(subGridParams);
+
+                //添加
+                GridLayout grid = (GridLayout) findViewById(R.id.mainPage_layout);
+                grid.addView(rv,childCount-1);
+            }
+        });
 
     }
 
