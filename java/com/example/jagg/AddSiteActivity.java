358bc1b97c18bb4616992e032d98cb351b27f33a
liu <liuyiguang8018@gmail.com>
2019-12-09 19:38:16 +0800

    ...

---
+++ java/com/example/jagg/AddSiteActivity.java
@@ -0,0 +1,44 @@
+package com.example.jagg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddSiteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_site);
+
+        final EditText et_name = (EditText)findViewById(R.id.add_site_name);
+        final EditText et_url = (EditText)findViewById(R.id.add_site_url);
+        Button bt_cancel = (Button)findViewById(R.id.add_site_cancel);
+        Button bt_ok = (Button)findViewById((R.id.add_site_ok));
+
+        //“取消”按钮
+        bt_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //“确认”按钮
+        bt_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = getIntent();
+                intent.putExtra("siteName", et_name.getText().toString());
+                intent.putExtra("siteUrl", et_url.getText().toString());
+                setResult(1, intent);
+                finish();
+            }
+        });
+
+    }
+}
--- java/com/example/jagg/MainActivity.java
+++ java/com/example/jagg/MainActivity.java
@@ -5,9 +5,17 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
@@ -26,6 +34,9 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -71,42 +82,9 @@ public class MainActivity extends AppCompatActivity {
         final RelativeLayout container = (RelativeLayout) grid.getChildAt(childCount-1);
         container.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
-                //10dp=1pixels
-                float pixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
-
-                //ImageView
-                ImageView iv = new ImageView(MainActivity.this);
-                iv.setId(R.id.reservedId);
-                iv.setBackgroundColor(Color.YELLOW);
-                RelativeLayout.LayoutParams imageViewParams = new RelativeLayout.LayoutParams(
-                        (int)(5*pixels),(int)(5*pixels));
-                imageViewParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-                imageViewParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
-                imageViewParams.setMargins((int)(0.8*pixels),(int)(0.8*pixels),(int)(0.8*pixels),(int)(0.8*pixels));
-                iv.setLayoutParams(imageViewParams);
-
-                //TextView
-                TextView tv = new TextView(MainActivity.this);
-                tv.setText("TEST");
-                RelativeLayout.LayoutParams textViewParams = new RelativeLayout.LayoutParams(
-                        RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
-                textViewParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
-                textViewParams.addRule(RelativeLayout.BELOW,R.id.reservedId);
-                tv.setLayoutParams(textViewParams);
-
-                //组合成RelativeLayout
-                RelativeLayout rv = new RelativeLayout(MainActivity.this);
-                GridLayout.LayoutParams subGridParams = new GridLayout.LayoutParams(
-                        GridLayout.spec(GridLayout.UNDEFINED,GridLayout.FILL,1f),
-                        GridLayout.spec(GridLayout.UNDEFINED,GridLayout.FILL,1f));
-                subGridParams.setGravity(Gravity.CENTER_HORIZONTAL);
-                rv.addView(iv);
-                rv.addView(tv);
-                rv.setLayoutParams(subGridParams);
-
-                //添加
-                GridLayout grid = (GridLayout) findViewById(R.id.mainPage_layout);
-                grid.addView(rv,childCount-1);
+                //启动addSiteActivity获取信息（图标颜色、网站名、网站链接）
+                Intent  intent=new Intent(MainActivity.this,AddSiteActivity.class);
+                startActivityForResult(intent,1);
             }
         });
 
@@ -119,5 +97,76 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode==1 && requestCode==1){
+            //接收返回数据
+            final String siteName = data.getStringExtra("siteName");
+            final String siteUrl = data.getStringExtra("siteUrl");
+
+            //10dp=1pixels
+            float pixels = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
+
+            //ImageView
+            ImageView iv = new ImageView(MainActivity.this);
+            iv.setId(R.id.reservedId);
+
+            Bitmap bitmap = Bitmap.createBitmap(200,200,Bitmap.Config.ARGB_8888);
+            bitmap.eraseColor(Color.parseColor("#e0ffff")); //填充颜色
+            Canvas canvas = new Canvas(bitmap);
+            Paint paint = new Paint();
+            paint.setTextSize(100);
+            paint.setColor(Color.BLACK);
+            canvas.drawText(siteName.substring(0,1),50,130,paint);
+
+            iv.setImageBitmap(bitmap);
+            RelativeLayout.LayoutParams imageViewParams = new RelativeLayout.LayoutParams(
+                    (int)(5*pixels),(int)(5*pixels));
+            imageViewParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+            imageViewParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+            imageViewParams.setMargins((int)(0.8*pixels),(int)(0.8*pixels),(int)(0.8*pixels),(int)(0.8*pixels));
+            iv.setLayoutParams(imageViewParams);
+
+            //TextView
+            TextView tv = new TextView(MainActivity.this);
+            tv.setText(siteName);
+            tv.setContentDescription(siteUrl);
+            RelativeLayout.LayoutParams textViewParams = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+            textViewParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+            textViewParams.addRule(RelativeLayout.BELOW,R.id.reservedId);
+            tv.setLayoutParams(textViewParams);
+
+            //组合成RelativeLayout
+            RelativeLayout rv = new RelativeLayout(MainActivity.this);
+            GridLayout.LayoutParams subGridParams = new GridLayout.LayoutParams(
+                    GridLayout.spec(GridLayout.UNDEFINED,GridLayout.FILL,1f),
+                    GridLayout.spec(GridLayout.UNDEFINED,GridLayout.FILL,1f));
+            subGridParams.setGravity(Gravity.CENTER_HORIZONTAL);
+            rv.addView(iv);
+            rv.addView(tv);
+            rv.setLayoutParams(subGridParams);
+
+            //添加
+            GridLayout grid = (GridLayout) findViewById(R.id.mainPage_layout);
+            int childCount = grid.getChildCount();
+            grid.addView(rv,childCount-1);
+
+            //设置点击事件
+            rv.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View view){
+                    //向webActivity发送信息并调用webActivity
+                    Intent intent = new Intent(MainActivity.this, WebActivity.class);
+                    intent.putExtra("siteUrl",siteUrl);
+                    intent.putExtra("siteName",siteName);
+                    startActivity(intent);
+                }
+            });
+        }
+
     }
 }
